41cba414650e6facd9780690fca70ca3956ff8e9
Max Netyaga <max.netyaga@gmail.com>
2025-05-27 22:19:39 +0300

    fix imports in server

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -9,8 +9,8 @@ import (
 	"strconv"
 	"time"
 
-	"github.com/maxnetyaga/architecture-practice-4-template/httptools"
-	"github.com/maxnetyaga/architecture-practice-4-template/signal"
+	"github.com/maxnetyaga/architecture-practice-5/httptools"
+	"github.com/maxnetyaga/architecture-practice-5/signal"
 )
 
 var port = flag.Int("port", 8080, "server port")
